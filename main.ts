4f19a5a5ea3ddecb15f86c624275619f97b9669d
Code Ninjas Garden Grove <68670709+codeninjasgg@users.noreply.github.com>
2025-06-08 14:04:28 -0700

    save images please

--- main.ts
+++ main.ts
@@ -35,7 +35,24 @@ function attachStatusBar (plant: Sprite) {
     statusbar.attachToSprite(plant)
 }
 function createPlayer () {
-    player = sprites.create(assets.image`player_female`, SpriteKind.Player)
+    player = sprites.create(img`
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f f . . . . . 
+        . . . . f 1 5 2 5 1 6 f . . . . 
+        . . . f 1 6 6 6 6 6 1 6 f . . . 
+        . . . f 6 6 f f f f 6 1 f . . . 
+        . . . f 6 f f d d f f 6 f . . . 
+        . . f 6 f d f d d f d f 6 f . . 
+        . . f 6 f d 3 d d 3 d f 6 f . . 
+        . . f 6 6 f d d d d f 6 6 f . . 
+        . f 6 6 f 3 f f f f 3 f 6 6 f . 
+        . . f f d 3 5 3 3 5 3 d f f . . 
+        . . f d d f 3 5 5 3 f d d f . . 
+        . . . f f 3 3 3 3 3 3 f f . . . 
+        . . . f 3 3 5 3 3 5 3 3 f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
     tiles.placeOnTile(player, tiles.getTileLocation(1, 1))
     controller.moveSprite(player)
     scene.cameraFollowSprite(player)
@@ -58,28 +75,79 @@ if (alreadyPlanted) {
         return
     }
     if (seedToPlant == "carrot") {
-        plant = sprites.create(assets.image`seed_carrot`, SpriteKind.Plant)
+        plant = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . 4 . . . . . . 4 . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 4 . . 4 . . 4 . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . 4 . . . . . 4 . . . 4 . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 4 . . . . 4 . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Plant)
         tiles.placeOnTile(plant, player.tilemapLocation())
     } else if (seedToPlant == "apple") {
-        plant = sprites.create(assets.image`seed_carrot`, SpriteKind.Plant)
+        plant = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . 2 . . . . . . . 2 . . . 
+            . . . . . . . . 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . 2 . . . . . . . . . . . . . 
+            . . . . . . . 2 . . . 2 . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . 2 . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . 2 . . 2 . . 2 . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . 2 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Plant)
         tiles.placeOnTile(plant, player.tilemapLocation())
     } else if (seedToPlant == "eggplant") {
-        plant = sprites.create(assets.image`seed_carrot`, SpriteKind.Plant)
+        plant = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . a . . . . . . . a . . . . 
+            . . . . . . . a . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . a . . . 
+            . . . . a . . . a . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . a . . . . 
+            . . . . . . . . . . . . . . a . 
+            . . . . . . . a . . . . . . . . 
+            . . . a . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . a . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Plant)
         tiles.placeOnTile(plant, player.tilemapLocation())
     } else {
     	
     }
     seedsPlanted.push(plant)
 }
-let mySprite: Sprite = null
-let CURRENT_LEVEL = 0
-let seedsPlanted: Sprite[] = []
-let player: Sprite = null
-let seed_carrot = null
-let seed_cabbage = null
-let plant: Sprite = null
 let statusbar: StatusBarSprite = null
+let mySprite: Sprite = null
 let seedToPlant = ""
+let plant: Sprite = null
+let seed_cabbage = null
+let seed_carrot = null
+let player: Sprite = null
+let seedsPlanted: Sprite[] = []
+let CURRENT_LEVEL = 0
 seedToPlant = "carrot"
 namespace StatusBarKind {
     export const Growth = SpriteKind.create();
@@ -92,62 +160,6 @@ enum PlantState {
     ripening,
     ripening_watered,
     fruiting
-}
-class Plant {
-    statusbar: StatusBarSprite = null;
-    type: PlantType = null;
-    sprite: Sprite = null;
-    state: PlantState = null;
-
-    constructor(plantType: PlantType) {
-        this.type = plantType;
-        this.createSprite();
-    }
-
-    private attachTimerStatusBar() {
-        this.statusbar = statusbars.create(15, 4, StatusBarKind.Growth);
-        this.statusbar.setBarBorder(1, 15);
-        this.statusbar.setColor(7, 2);
-        this.statusbar.value = 10;
-        this.statusbar.positionDirection(CollisionDirection.Top);
-        this.statusbar.attachToSprite(plant);
-    }
-
-    private setTimerValue(time: number) {
-        this.statusbar.value = time;
-    }
-
-    private createSprite() {
-        if (seedToPlant == "carrot") {
-            this.sprite = sprites.create(assets.image`seed_carrot`, SpriteKind.Plant)
-        } else if (seedToPlant == "apple") {
-            this.sprite = sprites.create(assets.image`seed_tomato`, SpriteKind.Plant)
-        } else if (seedToPlant == "eggplant") {
-            this.sprite = sprites.create(assets.image`seed_crab_apple`, SpriteKind.Plant)
-        }
-        if (this.sprite) {
-            tiles.placeOnTile(this.sprite, player.tilemapLocation())
-        }
-    }
-
-    public water() {
-        switch (this.state) {
-            case PlantState.planted:
-                this.state = PlantState.planted_watered;
-            case PlantState.seedling:
-                this.state = PlantState.seedling_watered;
-            case PlantState.ripening:
-                this.state = PlantState.ripening_watered;
-            case PlantState.fruiting:
-                this.state = PlantState.fruiting;
-            default:
-                break;
-        }
-    }
-
-    public grow() {
-        // whenever the plant is watered, grow the plant
-    }
 }
 let playerSelectSeedTexts = [
 "Oh cool!",
@@ -173,7 +185,24 @@ tiles.setCurrentTilemap(tilemap`level1`)
 createPlayer()
 let column = 4
 for (let index = 0; index < 3; index++) {
-    mySprite = sprites.create(assets.image`strawberry`, SpriteKind.Plant)
+    mySprite = sprites.create(img`
+        . . . . . . . 6 . . . . . . . . 
+        . . . . . . 8 6 6 . . . 6 8 . . 
+        . . . e e e 8 8 6 6 . 6 7 8 . . 
+        . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+        . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+        . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+        e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+        e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+        e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+        e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+        e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+        e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+        e e 2 e 2 2 4 2 2 e e e c . . . 
+        e e e e 2 e 2 2 e e e c . . . . 
+        e e e 2 e e c e c c c . . . . . 
+        . c c c c c c c . . . . . . . . 
+        `, SpriteKind.Plant)
     tiles.placeOnTile(mySprite, tiles.getTileLocation(column, 1))
     column += 1
     attachStatusBar(mySprite)
